7e287bfa878fd2e0b748b1408126ccd35c63626b
Juan Avila <ag.juandiego@gmail.com>
2022-01-29 14:23:25 -0500

    feat: add header section

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header/header.js';
 
-ReactDOM.render( <
-    div / > ,
+ReactDOM.render( 
+    <div className="app">
+        <div><Header /> </div>
+    </div>,
     document.getElementById('root')
 );
 
